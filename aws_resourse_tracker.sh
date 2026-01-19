05758b0497dc433d97c306753f2da4d0a7beaed9
Rohan Deuja <rrohandeuja@gmail.com>
2026-01-18 23:14:21 -0500

    write a shell script

---
+++ aws_resourse_tracker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##############
+# Author : Rohan Deuja
+# Date : 18th jan
+#
+# This script will report the AWS resource usage
+# ##################
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+# List S3 buckets
+aws s3 ls
+
+# List EC2 instances
+aws ec2 describe-instances
+
+# List lamda
+aws lambda list-functions
+
+# List IAM Users
+aws iam list-users
